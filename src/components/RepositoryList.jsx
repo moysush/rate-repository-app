e014d79802387805fb84d65dd541da535ed078f5
Mahmud Hossain Sushmoy <m.hossain.sushmoy@gmail.com>
2026-03-09 05:07:23 +0600

    Exercise 10.11: fetching repositories with Apollo Client

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,38 @@
-import { FlatList } from "react-native";
+import { FlatList, View, ActivityIndicator } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
+import Text from "./Text";
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const { repositories, loading, error } = useRepositories();
 
   const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
+    ? repositories?.edges?.map((edge) => edge.node)
     : [];
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          padding: 16,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>Loading repositories...</Text>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={{ padding: 16 }}>
+        <Text color="error">Failed to load repositories.</Text>
+        <Text>{error.message}</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,23 +1,25 @@
+// import { useEffect, useState } from "react";
 import { useEffect, useState } from "react";
+import { GET_REPOSITORIES } from "../graphql/queries";
+import { useQuery } from "@apollo/client";
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, loading, error, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+  // const response = await fetch("http://192.168.1.29:5000/api/repositories");
+  // const json = await response.json();
 
-  const fetchRepositories = async () => {
-    const response = await fetch("http://192.168.1.29:5000/api/repositories");
-    const json = await response.json();
-
-    // console.log(json.edges);
-    setLoading(false);
-    setRepositories(json);
-  };
+  // console.log(JSON.stringify(data, null, 2));
 
   useEffect(() => {
-    fetchRepositories();
-  }, []);
+    if (data?.repositories) {
+      setRepositories(data.repositories);
+    }
+  }, [data]);
 
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, error, refetch };
 };
 
 export default useRepositories;
